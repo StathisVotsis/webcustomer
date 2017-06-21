ea7784692747d626d49cc15bbc0aeaeeb8bb65cf
Stathis Votsis <stathisvotsis@gmail.com>
2017-06-21 23:33:55 +0300

    end

---
+++ webcustomer/Stathis.Repository/CustomerRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stathis.Data;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Stathis.Repository
+{
+    public class CustomerRepository
+    {
+        public List<Customer> GetCustomers()
+        {
+            //return listOfCustomers.  
+            var customers = new List<Customer>();
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["db"].ToString()))
+            {
+                SqlCommand sqlCmd = new SqlCommand();
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.CommandText = "Select * from Table2;";
+                sqlCmd.Connection = connection;
+                connection.Open();
+                using (var reader = sqlCmd.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    {
+
+                        var customer = new Customer();
+                        customer.Id = Int32.Parse(reader["Id"].ToString());
+                        customer.FirstName = reader["FirstName"].ToString();
+                        customer.Email = reader["Email"].ToString();
+                        customers.Add(customer);
+
+                    }
+                }
+
+            }
+
+            return customers;
+        }
+
+        public Customer GetById(int Id)
+        {
+            var customer = new Customer();
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["db"].ToString()))
+            {
+
+                SqlCommand sqlCmd = new SqlCommand();
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.CommandText = "Select * from Table2 where Id='" + Id + "';";
+                sqlCmd.Connection = connection;
+                connection.Open();
+                //SqlDataReader reader = sqlCmd.ExecuteReader();
+                using (var reader = sqlCmd.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    {
+                        customer.Id = Int32.Parse(reader["Id"].ToString());
+                        customer.FirstName = reader["FirstName"].ToString();
+                        customer.Email = reader["Email"].ToString();
+                    }
+                }
+            }
+            return customer;
+        }
+    }
+}
---
+++ webcustomer/webcustomer/Controllers/StathisController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Stathis.Data;
+using Stathis.Repository;
+
+namespace webcustomer.Controllers
+{
+    [RoutePrefix("api/webcustomer")]
+    public class StathisController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public HttpResponseMessage GetAll()
+        {
+            var customerRepo = new CustomerRepository();
+            //return customerRepo.GetCustomers();
+            return Request.CreateResponse(HttpStatusCode.OK, customerRepo.GetCustomers());
+        }
+
+        [Route("{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage GetById(int id)
+        {
+            var customerRepo = new CustomerRepository();
+            return Request.CreateResponse(HttpStatusCode.OK, customerRepo.GetById(id));
+
+        }
+    }
+}
